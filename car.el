a16d317aef8b3ef5610023681309da4d529950a1
DolphinShark20 <nomail@nomail.com>
2025-02-12 18:10:30 +0000

    Finalises car.el through breaking up the main function

--- car.el
+++ car.el
@@ -5,34 +5,49 @@
 
 (require 'comint)
 
-(defun car-exec ()
-  "Compile and run C code that is within the current buffer."
+(defun compile-c-file ()
+  "Compiles the current C file."
   (interactive)
   (if (eq major-mode 'c-ts-mode)
-    (progn
-      (save-buffer)
-      (let (
-	    (justname (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
-	    (orig-buf-name (buffer-name (current-buffer)))
-	    )
-	(compile (concat "gcc -O0 -g -o " justname " " justname ".c"))
-	(add-hook 'compilation-finish-functions
-		  (lambda (buf_obj out_str)
-		    (when (and
-			   (string-match "finished" out_str)
-			   (not (string-match "error" out_str))
-			   )
-		      (select-window buf_obj)
-		      (shell)
-		      (goto-char (point-max))
-		      (insert (concat justname ".exe"))
-		      (comint-send-input)
-		      (select-window (get-buffer-window orig-buf-name))
-		      ))
-		  )
+      (progn
+	(let (
+	      (buffer-c (buffer-name (current-buffer)))
+	      )
+	  (save-buffer)
+	  (compile (concat "gcc -O0 -g -o " (file-name-sans-extension buffer-c) " " buffer-c))
+	  )
 	)
-      )
-    (message "This isn't a buffer containing only C code.")))
+    (message "This does not seem to be a C buffer!")
+    )
+  )
+(global-set-key (kbd "C-c C--") 'compile-c-file)
+
+(defun compile-and-run-hook (comp_buf_obj comp_out_str)
+  "Use COMP_BUF_OBJ and COMP_OUT_STR to run Windows executable of compilation."
+  (when (and
+	 (string-match "finished" comp_out_str)
+	 (not (string-match "error" comp_out_str))
+	 )
+    (with-temp-buffer
+      (insert-buffer-substring comp_buf_obj)
+      (goto-char (point-min))
+      (re-search-forward "\\b\\(\\w+\\)\\.c\\b")
+      (let* (
+	     (output (match-string 1))
+	     (buf-c (concat output ".c"))
+	     )
+	(select-window (get-buffer-window comp_buf_obj))
+	(split-window-below nil (get-buffer-window comp_buf_obj))
+	(other-window 1)
+	(shell)
+	(goto-char (point-max))
+	(insert (concat output ".exe"))
+	(comint-send-input)
+	(select-window (get-buffer-window buf-c))
+	))
+    )
+  )
+(add-hook 'compilation-finish-functions 'compile-and-run-hook)
 
 (provide 'car)
 ;;; car.el ends here
