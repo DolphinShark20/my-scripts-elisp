ec0d37b6c8de4d14786ef7259e913d4f59f6e41a
DolphinShark20 <nomail@nomail.com>
2025-02-20 23:26:27 +0000

    Fixes outstanding issue with car.el regarding active directory

--- car.el
+++ car.el
@@ -3,7 +3,15 @@
 ;;; Short snippet of elisp code to improve my ability to write Lisp and serve as a useful function.
 ;;; Code:
 
+;;; UNINTENDED ISSUE: If you use 'compile-and-run-hook' by compiling a different C source file in a different directory, the shell that was initially created
+;;; (and that will be used to try and execute [newsource].exe) will still be within the directory of the original C source file. This'll cause it to try and run
+;;; the new executible created from [newsource].c within the directory that [originalsource].c and [originalsource].exe lived in.
+;;; This can be easily fixed by adding a check to see which directory we're currently in vs. what the shell is in.
+;;; FIXED: Simply changes directory to the current one. Little messier than before, but it's not a problem.
+
 (require 'comint)
+
+(defvar cur-exec-dir nil)
 
 (defun compile-c-file ()
   "Compiles the current C file."
@@ -16,6 +24,7 @@
 	  (save-buffer)
 	  (compile (concat "gcc -O0 -g -o " (file-name-sans-extension buffer-c) " " buffer-c))
 	  )
+	(setq cur-exec-dir (file-name-directory (buffer-file-name (current-buffer))))
 	)
     (message "This does not seem to be a C buffer!")
     )
@@ -37,11 +46,14 @@
 	     (buf-c (concat output ".c"))
 	     )
 	(select-window (get-buffer-window comp_buf_obj))
-	(if (not (and (string-match "*shell*" (buffer-name(window-buffer(next-window))))))
+	(if (not (and (string-match "*shell*" (buffer-name (window-buffer (next-window))))))
 	    (split-window-below nil (get-buffer-window comp_buf_obj))
 	  )
 	(other-window 1)
 	(shell)
+	(goto-char (point-max))
+	(insert (concat "cd " cur-exec-dir))
+	(comint-send-input)
 	(goto-char (point-max))
 	(insert (concat output ".exe"))
 	(comint-send-input)
