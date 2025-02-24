c7dc4d450e2fab03a757f08ca9b4dd8620724aed
DolphinShark20 <nomail@nomail.com>
2025-02-24 18:24:00 +0000

    Setup working MUD interface and cleanup

--- mude.el
+++ mude.el
@@ -4,23 +4,64 @@
 ;;; I'll use 'open-network-stream' primarily to communicate with the server.
 ;;; I'm quite new to TCP communications, telnet, and everything, so I'll have to study other scripts that do similar things.
 ;;; Code:
-(defvar mud-process nil)
+(require 'comint)
 
-(defcustom mud-worlds nil
-  "List of lists that contain MUD world info (alias for world, host, port)
-This list can be changed using a built in function."
-  :type '(repeat
-	  (vector :tag "MUD World"
-	   (string :tag "Alias")
-	   (string :tag "Hostname")
-	   (string :tag "Port")
-	   (string :tag "Character" :value nil)
-	   (string :tag "Password" :value nil)
-	   )
-	  )
-  :group 'mude
+;;; General Global Variables
+(defvar mud-net-process nil)
+(defvar mud-input-buffer nil)
+
+;;; INPUT MODE SET UP
+(defun mud-send-input-line ()
+  "Sends input from input buffer's active line to active MUD connection stored at 'mud-net-process'."
+  (interactive)
+  (let* (
+	(eol (point))
+	(sol (line-beginning-position))
+	(input-line (concat (buffer-substring eol sol) "\n"))
+	)
+    (process-send-string mud-net-process input-line)
+    (newline)
+    )
   )
 
+(defvar mud-input-mode-map
+  (let (
+	(binds (make-sparse-keymap))
+	)
+    (set-keymap-parent binds text-mode-map)
+    (define-key binds (kbd "<RET>") 'mud-send-input-line)
+    binds
+    )
+  )
+
+(defun mud-input-mode ()
+  "Major mode for the buffer that will provide an input to the MUD network process."
+  (setq major-mode 'mud-input-mode)
+  (setq mode-name "MUD-IN")
+  (use-local-map mud-input-mode-map)
+  )
+
+(defvar mud-display-mode-map
+  (let (
+	(binds (make-sparse-keymap))
+	)
+;;;    (define-key binds (kbd "s") 'scroll-down-command)
+;;;    (define-key binds (kbd "w") 'scroll-up-command)
+    (set-keymap-parent binds comint-mode-map)
+    binds
+    )
+  "Key bindings that will be used for 'mud-display-mode'.
+By default, exactly the same as comint mode."
+  )
+
+(defun mud-display-mode ()
+  "Major mode for the buffer that will display the MUDs output."
+  (setq major-mode 'mud-display-mode)
+  (setq mode-name "MUD-OUT")
+  (use-local-map mud-display-mode-map)
+  )
+
+;;; Persistent list containing data to use for quick reconnecting
 (defcustom last-world nil
   "List of information about the last MUD connected to through mude."
   :type '(list
@@ -30,82 +71,114 @@ This list can be changed using a built in function."
   :group 'mude
   )
 
-(defun m-alias ()
-  "Create a preset MUD world with an alias that can be easily accessed."
-  (interactive)
+;;; FUNCTIONS TO SET UP GAME BUFFERS
+(defun setup-mud-input-buffer ()
+  "Set up the input buffer to interact with the MUD network process."
   (let (
-	(alias (read-from-minibuffer "Alias: "))
-	(host (read-from-minibuffer "Host: "))
-	(port (read-from-minibuffer "Port: "))
-	(auto-log-status (read-from-minibuffer "Would you like to add an auto-login character (y/[anything else]): "))
-	(char-name nil)
-	(pass nil)
+	(mud-in-buf (get-buffer-create "*MUD Input!*"))
 	)
-    (if (string-equal auto-log-status "y")
-	(progn
-	  (setq char-name (read-from-minibuffer "Character Name: "))
-	  (setq pass (read-from-minibuffer "Password (which is stored plainly in the init file): "))
-	  )
-      )
-    (setq mud-worlds (append mud-worlds (vector alias host port char-name pass)))
-    (customize-save-variable 'mud-worlds mud-worlds)
-    (message "MUD world '%s' has been added to the saved MUD lists!" alias)
-  )
-)
-
-(defun m-close ()
-  "Closes the current MUD session."
-  (interactive)
-  (if (eq mud-process nil)
-      (message "There's no current MUD session!")
-    (progn
-      (let (
-	    (mud-window (get-buffer-window "MUD!"))
-	    )
-      (if mud-window
-	  (delete-window mud-window)
-	)
-      )
-      (delete-process mud-process)
-      (message "Current MUD process has been ended!")
+    (setq mud-input-buffer mud-in-buf)
+    (pop-to-buffer mud-in-buf)
+    (with-current-buffer mud-in-buf
+      (mud-input-mode)
       )
     )
   )
 
+(defun setup-mud-display-buffer (mud-proc)
+  "Set up the display buffer for MUD-PROC."
+  (set-process-filter mud-proc 'mud-network-process-filter)
+  (setq mud-net-process mud-proc)
+  (let (
+	(mud-proc-buf (process-buffer mud-proc))
+	)
+    (with-current-buffer mud-proc-buf
+      (mud-display-mode)
+      )
+    (pop-to-buffer mud-proc-buf)
+    )
+  )
 
-(defun mude ()
+;;; Filter functions
+(defun mud-network-process-filter (net-proc str-out)
+  "Filter function for the NET-PROC proccess that serves STR-OUT to display."
+  (with-current-buffer (process-buffer net-proc)
+    (goto-char (point-max))
+    (insert str-out)
+    )
+  (with-selected-window (get-buffer-window (process-buffer mud-net-process))
+		       (goto-char (point-max))
+		       )
+  )
+
+;;; CORE FUNCTIONS
+(defun mude-nl ()
   "Main function for the MUD client."
   (interactive)
   (let* (
 	(input-host (read-from-minibuffer "Hostname: "))
 	(input-port (read-from-minibuffer "Port: "))
-	(net-proc (open-network-stream "MUD-PROCESS" "MUD!" input-host input-port))
-	(m-window (split-window-right))
+	(net-proc (open-network-stream "MUD-PROCESS" "*MUD!*" input-host input-port))
 	)
-    (setq mud-process net-proc)
+    (setq mud-net-process net-proc)
+        
     (setq last-world (list input-host input-port))
     (customize-save-variable 'last-world last-world)
-    (with-current-buffer "MUD!"
-      (read-only-mode 1)
-      )
-    (set-window-buffer m-window "MUD!")
+    
+    (setup-mud-display-buffer net-proc)
+    (setup-mud-input-buffer)
     )
   )
 
 (defun mude-re ()
   "Reconnect to last-joined MUD world."
   (interactive)
-  (let* (
-	 (last-host (car last-world))
-	 (last-port (nth 1 last-world))
-	 (net-proc (open-network-stream "MUD-PROCESS" "MUD!" last-host last-port))
-	 (m-window (split-window-right))
+  ;;; Annoying hack --- didn't want to bring in cl-return to handle mid-function breaking if user opts "N", so I put this in instead.
+  (let (
+	(pass t)
+	)
+    (if (and
+	 (get-buffer "*MUD!*")
+	 (get-buffer "*MUD Input!*")
 	 )
-    (setq mud-process net-proc)
-    (with-current-buffer "MUD!"
-      (read-only-mode 1)
+	(if (string-equal-ignore-case (read-from-minibuffer "MUD session/buffers may still be open! Close them and reconnect (Y) or halt reconnection (N/ANYKEY); [Y/N]: ") "Y")
+	    (mude-close)
+	  (setq pass nil)
+	  )
       )
-    (set-window-buffer m-window "MUD!")
+    (if pass
+	(let* (
+	       (last-host (car last-world))
+	       (last-port (nth 1 last-world))
+	       (net-proc (open-network-stream "MUD-PROCESS" "*MUD!*" last-host last-port))
+	       )
+	  (setup-mud-display-buffer net-proc)
+	  (setup-mud-input-buffer)
+	  )
+      )
+    )
+  )
+
+(defun mude-close ()
+  "Closes the current MUD session and its relevant buffers/windows."
+  (interactive)
+  ;;; Since killing the buffer kills the associated process, may as well just /only/ kill the buffer.
+  (let* (
+	(mud-out-buf (process-buffer mud-net-process))
+	(mud-out-window (get-buffer-window mud-out-buf))
+	(mud-in-buf mud-input-buffer)
+	)
+    
+    (if mud-out-buf
+	(kill-buffer mud-out-buf)
+      )
+    (if mud-in-buf
+	(kill-buffer mud-in-buf)
+      )
+    (if mud-out-window
+	(delete-window mud-out-window)
+      )
+    (message "Active MUD processes/buffers/windows have been closed! (If there were any)")
     )
   )
 
